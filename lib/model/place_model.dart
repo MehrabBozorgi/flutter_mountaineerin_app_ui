ee7e33a908e826f91972d65960fa17e050a6cb63
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-09-05 08:32:30 +0430

    Part 1

---
+++ lib/model/place_model.dart
@@ -0,0 +1,25 @@
+class PlaceModel {
+  final int id;
+  final String title;
+  final int reviews;
+  final String difficulty;
+  final int elevation;
+  final double distance;
+  final double price;
+  final String description;
+  final String image;
+
+
+  PlaceModel({
+    required this.id,
+    required this.title,
+    required this.reviews,
+    required this.difficulty,
+    required this.elevation,
+    required this.distance,
+    required this.price,
+    required this.description,
+    required this.image,
+
+  });
+}
